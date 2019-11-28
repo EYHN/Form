8fea838de9305d4a1cc32bca47e4ecae244fd51b
EYHN <cneyhn@gmail.com>
2019-11-28 10:17:51 +0800

    fix(db): leveldb NotFoundError

--- server/models/Leveldb.ts
+++ server/models/Leveldb.ts
@@ -7,11 +7,19 @@ export default class Leveldb {
     this.database = levelup(leveldown(dirname))
   }
   get = async (key: string) => {
-    const buffer = await this.database.get(key);
-    if (buffer instanceof Buffer) {
-      return buffer.toString();
+    try {
+      const buffer = await this.database.get(key);
+      if (buffer instanceof Buffer) {
+        return buffer.toString();
+      }
+      return buffer;
+    } catch (err) {
+      if (err instanceof Error && err.name === "NotFoundError") {
+        return null;
+      }
+      throw err;
     }
-    return buffer;
+    
   };
   set = (key: string, value: string) => this.database.put(key, value);
   delete = (key: string) => this.database.del(key);
