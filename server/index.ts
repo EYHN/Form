fa8d0a6d024b79768c3a762ff3ab841d1b898f6d
EYHN <cneyhn@gmail.com>
2019-11-28 09:48:55 +0800

    chore(heroku): support heroku

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,6 @@ const app = new Koa();
 
 InjectionMiddlewares(app);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('form.app API server start success.')
 });
